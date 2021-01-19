95d9c953d9a4b0771b7780c0165709a9c5cd2a87
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-01-19 12:59:08 -0500

    commit tree 53893d4c5939ae35a63074973df70a8917cb18ca

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,27 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+
+class GetRequester
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        ruby_hash = JSON.parse(self.get_response_body)
+        ruby_hash
+    end
+
+
+
+end
+
+
